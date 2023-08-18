d254463910ddb8c05b7f53cd6a208607b6c982a6
katiamiyu <katiamiyu@gmail.com>
2023-08-18 11:53:57 +0000

    task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,9 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i = 1;
 		if (head != NULL)
 			while (head->prev != NULL)
-			{
 				head = head->prev;
-			}
 		while (head != NULL)
 		{
 			if (i == idx)
