1c08fce2d99937535d0290aa6c613c7371adbabf
katiamiyu <katiamiyu@gmail.com>
2023-09-08 13:20:23 +0100

    1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+  * hash_djb2 - hashing function
+  * @str: key value
+  * Return: str hash value
+  */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hsh;
+	int x;
+
+	hsh = 5381;
+	while ((x = *str++))
+	{
+		hsh = ((hsh << 5) + hsh) + c;
+	}
+
+	return (hsh);
+}
