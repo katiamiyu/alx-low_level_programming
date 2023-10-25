57e717a45e2df7d6e915375bff20e0cca19668f9
katiamiyu <katiamiyu@gmail.com>
2023-10-25 18:39:07 +0000

    error removal

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,47 +1,42 @@
 #!/usr/bin/python3
-"""Defines a function to determine the perimiter of an island"""
+"""Defines a function to find the perimiter of an island"""
 
 
 def island_perimeter(grid):
-    """Determine the perimeter of a grid"""
+    """Find the perimeter of a grid"""
 
     height = len(grid)
     if grid[0]:
         width = len(grid[0])
 
-    perimeter = 0
+    peri = 0
 
-    for row_i, row in enumerate(grid):  # for each row
-        for col_i, col in enumerate(row):  # checking each unit of row
+    for row_i, row in enumerate(grid):
+        for col_i, col in enumerate(row):
 
-            if col is 1:  # if index is land
+            if col is 1:
 
-                # check above block
                 if row_i is 0:
-                    perimeter = perimeter + 1
+                    peri = peri + 1
                 else:
                     if grid[row_i - 1][col_i] is not 1:
-                        perimeter = perimeter + 1
+                        peri = peri + 1
 
-                # check left block
                 if col_i is 0:
-                    perimeter = perimeter + 1
+                    peri = peri + 1
                 else:
                     if grid[row_i][col_i - 1] is not 1:
-                        perimeter = perimeter + 1
+                        peri = peri + 1
 
-                # check right block
                 if col_i is (width - 1):
-                    perimeter = perimeter + 1
+                    peri = peri + 1
                 else:
                     if grid[row_i][col_i + 1] is not 1:
-                        perimeter = perimeter + 1
-
-                # check below block
+                        peri = peri + 1
                 if row_i is (height - 1):
-                    perimeter = perimeter + 1
+                    peri = peri + 1
                 else:
                     if grid[row_i + 1][col_i] is not 1:
-                        perimeter = perimeter + 1
+                        peri = peri + 1
 
-    return perimeter
+    return peri
