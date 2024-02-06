675f6bc8b05279121eef9d3d2f47316004743f09
katiamiyu <katiamiyu@gmail.com>
2024-02-06 16:10:32 +0000

    search test and betty cleared

---
+++ 0x1E-search_algorithms/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
+
+#endif
