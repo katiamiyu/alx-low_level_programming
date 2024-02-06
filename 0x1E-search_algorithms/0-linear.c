62b335169136ead430dc1a77b5002912c7470b27
katiamiyu <katiamiyu@gmail.com>
2024-02-06 15:57:05 +0000

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * linear_search - Searches linnear array and prints result
+  * @array: search array
+  * @size: array size
+  * @value: Value
+  * Return: Returns the first index of the value
+  */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int index;
+
+	if (array == NULL)
+		return (-1);
+
+	for (index = 0; index < (int) size; index++)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
