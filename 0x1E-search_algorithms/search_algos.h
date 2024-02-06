a6f0806d94379f8feb089ee7de59f032fde1246a
katiamiyu <katiamiyu@gmail.com>
2024-02-06 16:13:02 +0000

    search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
+
+#endif
